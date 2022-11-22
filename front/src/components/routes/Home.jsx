0eff7b62f4c3966a471207aa6d27d1c3d629c612
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-11-22 16:56:52 -0300

    estilo quoteForm y home

--- front/src/components/routes/Home.jsx
+++ front/src/components/routes/Home.jsx
@@ -35,17 +35,28 @@ const Home = () => {
 
   return (
     <div className="home">
-      <h2 className="titulo1">¡Conocé el estado de tu envío!</h2>
-      <select className="select" placeholder='Tipo de envío'>
+      <div className='search'>
+      <h1 className="titulo1">¡Conocé el estado de tu envío!</h1>
+      <br/>
+      <div class="search">
+      <select class="search__input" placeholder='Tipo de envío'>
         <option value= "enc">Encomienda</option>
         <option value= "sob" >Sobre</option>
         <option value= "cad">Carta documento</option>
         <option value= "sobi">Sobre internacional</option>
         <option value= "eco">E Commers</option>
       </select>
-      <input
-      type = 'text'
-      placeholder="ingresa el numero de seguimiento"/>
+    <input type="text" class="search__input" placeholder='Tipo de envío'/>
+    <button class="search__button">
+        <svg class="search__icon" aria-hidden="true" viewBox="0 0 24 24">
+            <g>
+                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+            </g>
+        </svg>
+    </button>
+</div>
+     
+      </div>
        <h2>¿Qué querés enviar?</h2>
        <h4>Múltiples servicios para tus múltiples nesecidades</h4>
         <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAbVJREFUSEu1lYExBEEQRf9FgAjIABGQASJABIgAESACREAGyIAMZIAIqLfV/6qva3btKjdVVzfTO9O/+//umZmWPGZL9q8pALuSziVtSDqW9DwmuDEAOMTxUXEIwJmk1yGgIYBVSSeSLsLBV5pjWwn7naRLSe8toD6A04gaEI8DSY+xwO49mD4lXUu6ifn8UAXYl3QVPLPpXhLcr8cJaIF/Rwt9ZHMY3wECmHPdyABEtxf2lzhoITmUaSFaaMEhgyD4vhNrdNmuAN+JDhzjIFcKtOAEXUwLa2hxhfG/EHzOwACIOSQgtCCso4UubIx8tvPdAlgbISDZ3EpCMw8yhrqPTH8LwLY+AU1VrrAcrJnozaBWVhXQEbsQnkrBTAawQzoZWhiUKjowFhzW9RBFid75tDrjA5VGNbnifs0gd2wFaQHkPYj+0CcyzbEZH1t90KLDzmsfvEnaqmXKGp4ptb4+qBnUm5Y+oOutT/M98EXGRgO5Y13j9Eq9aQmMn6+PLruh67rVB659nHjOxUYAk67rLBxcEpmvhtwHZPnnB6dWEEKaW7T6tyez1Q+jbWPe5NHOWht/ACgGaxmb7/TKAAAAAElFTkSuQmCC"className='iconos'/>
@@ -54,7 +65,7 @@ const Home = () => {
         <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAPJJREFUSEvtldsRwUAUhr9UgAqUgEoogQ6oIDqhBDqhAypABcyf2TVn4uQikZeMfcrsnvPf9pKEjkfSMT79IlgAKTBtGdsF2AAH4diItDBuCR7bT8AsT/AMq0dg3pDI9mbirYNIoDnFtQcGNYkewDLEYnFcghFwB4aBpMqNVAs89tyCqEIHKlzFTSpxY1ULU653Qdg7HS+imIpOgYg8N3nVAhaBHYUObJHnRuvZEXRUf03gudGc9sdT3ZhAjdGNvm3WRQetVkQ1T6lb9ieoTO8jol8+duf4KtuLpouyBSaV2soLrsDae65b4vrt/fpldhLRC6AYNhngkXrwAAAAAElFTkSuQmCC"className='iconos'/>
         <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAV9JREFUSEvllP0tRFEQR89WgArQARWgAlSAClABKqADVGBVgAroAB1QATly5+W+8Z68fckmxPyz2dk7c+bjNzthzjaZc376ABfA4YxwY45zTBfgFDiZMXk8PwOMbywDVoBHYBHYAu6BHeAGeAA2S6T+DWAXmBb/HfAGrAMvQcgAH28DtyWx76KjurouX8T6KfjLaoDVWcU7sFZVkas1rqsru38CFqruWwDbWgbyHG3boNUKarLnMpKlauTRmbl83wL8lMi3XQD9AozVAuwU3GMLcAXslaWF3C7TYp2tgdl/UADnZXzXwH4GBD0r1Go0x1Rbn7/VbVbRR5XhtSzNSqzaQ1Jhmio7KqOxc0Xh/sKavH2AsX8hUeDvAngfLtUd1aYUXa53Ejaqg7iPvHy/N3ovP44CfAvqSwb8A0D8XXTNe6jP+2kEkfWuYjyc+miGJvadyT3MRlljD2ow9O8DPgHWLF8ZxUgN9QAAAABJRU5ErkJggg=="className='iconos'/>
        
-       <h3>¡Cotiza tu envío en dos simples pasos!</h3>
+       <h1 className="titulo1">¡Cotiza tu envío en dos simples pasos!</h1>
        
        <form  onSubmit={handleSubmit(onSubmit)}>
         <h2>Informacion del envío</h2>
@@ -130,7 +141,7 @@ const Home = () => {
           <button className="login__btn">Siguiente paso</button>
        </form>
 
-       <h3>¿Ya conoces los planes personalizados que tenemos para vos?</h3>
+       <h1 className="titulo1">¿Ya conoces los planes personalizados que tenemos para vos?</h1>
 
       <div className='contenedorTarjeta'>
        <div className='tarjeta'>
@@ -144,25 +155,21 @@ const Home = () => {
         </div>
         </div>
 
-        <h3>Nuestras sucursales</h3>
+        <h1 className='titulo1'>Nuestras sucursales</h1>
 
-        <h4>Retira y despacha donde mas comodo te quede</h4>
+        <h3 className='titulo2'>Retira y despacha donde mas comodo te quede</h3>
 
-        <h3>¿Como lo hago?</h3>
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAVFJREFUSEu9lWFRAzEQhb8qAAcUBQUFUAegoHUAVUBRACgAFEAVFAngAByAAjpvJuns7e31kjJHZvLrsu/bvOzujRh4jQbWpwQwB86Ak7SV03var8BqV5K7ABfAHTDuueUnsAAEa60uwD1wVWmfYgRqrAiwj3gWfQCuLcEDZMtLZeb++KW1ywPk59EfAdI4zhoWoGp5rBD/AWTnTRCzvYUFPAGzQoDEz1OpLgPIM6CEG32g2p4UAKy4emMNHLo4aZ16wG8gfpuq4iB9KxFv2G8t8gCJ6/rK8i1FZVu6Mm9VqAV4i76BafJZglo6UyL+kcdK3yNbiAAl4joXPnJXk2WIAqMHjeoiLFMd7Go0QbR8tUTiX3ZA/vuoUEaDDrt85X0grUnqG837qUcXqG/4yXON6KofjoUJpK0SzaNEda6ekGgoHE3TgjFUf6Tkp1+vaiI25jFGGZVkHskAAAAASUVORK5CYII="/>
-        <h4 className='inLine'>Selecciona el tipo de envío</h4>
-        <br/>
-        
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAVFJREFUSEu9lWFRAzEQhb8qAAcUBQUFUAegoHUAVUBRACgAFEAVFAngAByAAjpvJuns7e31kjJHZvLrsu/bvOzujRh4jQbWpwQwB86Ak7SV03var8BqV5K7ABfAHTDuueUnsAAEa60uwD1wVWmfYgRqrAiwj3gWfQCuLcEDZMtLZeb++KW1ywPk59EfAdI4zhoWoGp5rBD/AWTnTRCzvYUFPAGzQoDEz1OpLgPIM6CEG32g2p4UAKy4emMNHLo4aZ16wG8gfpuq4iB9KxFv2G8t8gCJ6/rK8i1FZVu6Mm9VqAV4i76BafJZglo6UyL+kcdK3yNbiAAl4joXPnJXk2WIAqMHjeoiLFMd7Go0QbR8tUTiX3ZA/vuoUEaDDrt85X0grUnqG837qUcXqG/4yXON6KofjoUJpK0SzaNEda6ekGgoHE3TgjFUf6Tkp1+vaiI25jFGGZVkHskAAAAASUVORK5CYII="/>
-        <h4 className='inLine'>Elegí tu ciudad mas cercana</h4>
-        <br/>
-        
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAVFJREFUSEu9lWFRAzEQhb8qAAcUBQUFUAegoHUAVUBRACgAFEAVFAngAByAAjpvJuns7e31kjJHZvLrsu/bvOzujRh4jQbWpwQwB86Ak7SV03var8BqV5K7ABfAHTDuueUnsAAEa60uwD1wVWmfYgRqrAiwj3gWfQCuLcEDZMtLZeb++KW1ywPk59EfAdI4zhoWoGp5rBD/AWTnTRCzvYUFPAGzQoDEz1OpLgPIM6CEG32g2p4UAKy4emMNHLo4aZ16wG8gfpuq4iB9KxFv2G8t8gCJ6/rK8i1FZVu6Mm9VqAV4i76BafJZglo6UyL+kcdK3yNbiAAl4joXPnJXk2WIAqMHjeoiLFMd7Go0QbR8tUTiX3ZA/vuoUEaDDrt85X0grUnqG837qUcXqG/4yXON6KofjoUJpK0SzaNEda6ekGgoHE3TgjFUf6Tkp1+vaiI25jFGGZVkHskAAAAASUVORK5CYII="/>
-        <h4 className='inLine'>Conoce nuestra sucursal mas cercana</h4>
+        <h1 className='titulo1'>¿Como lo hago?</h1>
+        <ul>
+          <li>Selecciona el tipo de envío</li>
+          <li>Elegí tu ciudad mas cercana</li>
+          <li>Conoce nuestra sucursal mas cercana</li>
+        </ul>
+       
         <br/>
         <div className='inLine'>
         <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAbBJREFUSEu1le01REEQRO9GgAgQASJABIgAESASRIAIEAEiIANkQASc63Q7s+PNm7c/zJ+3Zz6qqqt6Zmf885j9Mz5TCNaAE2AH2AxBL8AjcAm8jYnsEVwE+BiGe85aG8YIVLkRB28AgZxzWMkpcFhUtDVE0iJI5Z9hTQLXGBJp1VLYJencGCLQ89fYpSrBnTuvMtAW/ZfkOfav15kMEaR6A1SR4AIsV+I+AAVIch125ZnfrUME6X2qvwP2gCfgKE4KuA3cA/tFFZ6dy2KI4CtAck2lelyWnzaq3nlHfe5nchGCFUAyRxK8x++FCOwKy68tcv44CK6iu9IiL+FD2Ojv0QwyMLvEwFWrt9pUDlvYDtImm8Eu875kTk2LDO02Dqa/kkhWPhWC5jPhdxU4AGyK0QpczANaYkVjQ8VaVubRJcgqDNUqMtyayLvhpfT7R32rixIkw/a72ygh74x3ZC7c3D/22KlKqwxXm7KD8mw2g2Gb0WCVvee6fMxKkhJc5a3HcNIfTk1iZWbUe2mbbTpkd0ni+iTwXsg1kSTZsrZm05Yp96DT+tOXeyFPR2rs/AZz92gZw5u8WQAAAABJRU5ErkJggg=="/>
-       <button className='inLine'>Buscar sucursal</button>
+       <button className='login__btn1'>Buscar sucursal</button>
        </div>
 
        <h2>¡Registrate y conocé todo lo que podes hacer con tus envios!</h2>
@@ -177,11 +184,11 @@ const Home = () => {
         <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAPNJREFUSEvtldENwjAQQ90JgAmACYANYBOYDDaBEWADmAA2ALlKpOi4pE6lSHzQv/ZyflbiSzs0frrG+qgBTAGcg6EdgJdiTgVE8XUQvQKQIAogFb8FwAqABBkCWPFtAFwASJASwBOP+86aBMkBSuLxbCWIB1DEZYgF1IhLkBRgozhTsw6AvU8vwimAsWMy4jOUMDtn7+QDtTZ8bwXgvPRD6bmMTmKN2T8CWBjLdwCHEFeWbF+/XAFQaJ65d1hbhtpogNvoOP4Dvs7LbsnvbBFvzInyt3LWPGycvZgy96dCNHNsiu+TucjOwUjzflvtfVMN/wCUvlMZx+RBFQAAAABJRU5ErkJggg=="/>
         <h3>Cambia el método de envío</h3>
 
-        <button>Registrate ahora</button>
+        <button className='login__btn1'>Registrate ahora</button>
 
         <h2>¿Nesecitas ayuda?</h2>
         <h4>Lee nuestras preguntas frecuentes y resolvé tus dudas</h4>
-        <button>Preguntas frecuentes</button>
+        <button className='login__btn1'>Preguntas frecuentes</button>
 
         <h2>Nuestras redes</h2>
         <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAUdJREFUSEvdleExBEEQhb+LgAzIABEgAkTgRIAIEAEhyAARkAGXAREgAuq7mrmanZuZ29uq/XNTdX92ut/r97qnb8LIZzIyPptHsA38rLDNmEvgFNgHPoFH4C7k+f05YuQWGWzAR4PkDTgs3Jsj4TXwUCLw8j0oOAMEyk+MqfFLMgsE8yJzBX9JplUoO7VMdU8Ndb/BgUVxOYHenWQAfvOnfQfAfYPgJRAsQlICm7cbwHYGvg8V36a5OcH3QOCY1mlwqQfaMLR68bSwM4F5D5wSG7Q1QMlXsLiTWloVTsp53qwehEv2lCzy2xHw2gMwDbF61S9tgdqycxJu1iA5rjzM5jZ1bH1s2tU6F2EXFWNqCrQpLrQauK/2qgWe90BQK97rYY0vVnDHunlKCqah0TYtkrnAbGC6NlZhz+837x+tl+x1gka36B/CnzgZFPaBJAAAAABJRU5ErkJggg=="/>
