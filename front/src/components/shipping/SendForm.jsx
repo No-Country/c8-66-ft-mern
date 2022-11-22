8172e05e7eaec0ccf6bea4d16c038c01081595a2
kelvynloreto <loretokelvyn@gmail.com>
2022-11-22 16:40:58 -0400

    add client address (ubigeo)

--- front/src/components/shipping/SendForm.jsx
+++ front/src/components/shipping/SendForm.jsx
@@ -1,99 +1,125 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import AddressClient from "./AddressClient";
 
 const SendForm = (data) => {
+  const [toggleAddress, setToggleAddress] = useState();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-    const { register, handleSubmit,formState:{errors}, reset } = useForm();
+  const postForm = (data) => {
+    const url = `http://localhost:4000/api/v1/`;
+    axios
+      .post(url, data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const submit = (data) => {
+    postForm(data);
+  };
 
-    const postForm= (data)=>{
-        const url = `http://localhost:4000/api/v1/`;
-        axios.post(url, data)
-          .then((res) => {
-            console.log(res.data);
-          })
-          .catch((err) => console.log(err)
-          )
-    }
-const submit = (data)=>{
-    postForm(data)
-}
-    
+  const handleChange = () => {
+    setToggleAddress(event.target.value);
+  };
   return (
-    <form onSubmit={handleSubmit(submit)} className='send_form'>
-       <section>
+    <form onSubmit={handleSubmit(submit)} className="send_form">
+      <section>
         <input
-          {...register("name",{
-            required: true
+          {...register("name", {
+            required: true,
           })}
           type="text"
           id="name"
           placeholder="Nombre y Apellido / Razón Social"
         />
-        {errors.name?.type === 'required' && <p>Campo requerido</p>}
-        </section>
-       <section>
+        {errors.name?.type === "required" && <p>Campo requerido</p>}
+      </section>
+      <section>
         <input
-          {...register("phone",{
-            required: true
+          {...register("phone", {
+            required: true,
           })}
           type="text"
           id="phone"
           placeholder="94232234"
         />
-        {errors.phone?.type === 'required' && <p>Campo requerido</p>}
-        </section>
-        <section>
-        <input 
-          {...register("email",{
+        {errors.phone?.type === "required" && <p>Campo requerido</p>}
+      </section>
+      <section>
+        <input
+          {...register("email", {
             required: true,
-            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i 
+            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
           })}
-          type="text" 
+          type="text"
           id="email"
           placeholder="email"
         />
-        {errors.email?.type === 'required' && <p>Campo requerido</p>}
+        {errors.email?.type === "required" && <p>Campo requerido</p>}
       </section>
 
       <section>
         <select
-          {...register("order_option",{
-            required: true
+          {...register("order_option", {
+            required: true,
           })}
-          id="order_option" 
-        > 
-        <option hidden selected>Como nos entregas tu pedido?</option>
-        <option value="1">Pick up en mi domicilio</option>
-        <option value="2">En una surcursal</option>
+          id="order_option"
+          onChange={handleChange}
+        >
+          <option hidden selected>
+            Como nos entregas tu pedido?
+          </option>
+          <option value="domicilio">Pick up en mi domicilio</option>
+          <option value="surcursal">En una surcursal</option>
         </select>
-        {errors.order_option?.type === 'required' && <p>Campo requerido</p>}
-        </section>
-
-        <section>
-        <input 
-          {...register("postal_code",{
-            required: true,
-
-          })}
-          type="text" 
-          id="postal_code"
-          placeholder="Codigo Postal"
-        />
-        {errors.postal_code?.type === 'required' && <p>Campo requerido</p>}
+        {errors.order_option?.type === "required" && <p>Campo requerido</p>}
       </section>
-
+      <div className={toggleAddress === "surcursal" ? "off" : "on"}>
         <section>
-        <textarea  
-          {...register("comments",{
+          <input
+            {...register("address", {
+              required: true,
+            })}
+            type="text"
+            id="address"
+            placeholder="direccion de retiro del envio"
+          />
+          {errors.address?.type === "required" && <p>Campo requerido</p>}
+        </section>
+        <section>
+          <input
+            {...register("postal_code", {
+              required: true,
+            })}
+            type="text"
+            id="postal_code"
+            placeholder="Codigo Postal"
+          />
+          {errors.postal_code?.type === "required" && <p>Campo requerido</p>}
+        </section>
+      </div>
+
+      <section>
+        <textarea
+          {...register("comments", {
             required: true,
           })}
-          type="text" 
+          type="text"
           id="comments"
           placeholder="Quieres hacer una aclaración?"
         />
       </section>
-    </form>
-  )
-}
 
-export default SendForm
+      <AddressClient/>
+    </form>
+  );
+};
+
+export default SendForm;
