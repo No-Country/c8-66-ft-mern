0f573798056bdbfce139a76738f2dc746b6c7120
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-12-01 12:23:40 -0300

    fix:config sequelize

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config({path:'./config.env'});
 
 //Config DB
 const host = process.env.DB_HOST;
-const username = process.env.DB_USER || 'root';
-const database = process.env.DB_NAME;
-const password = process.env.DB_PASS;
+const username = process.env.DB_USERNAME || 'root';
+const database = process.env.DB;
+const password = process.env.DB_PASSWORD;
 const dialect = process.env.DB_TYPE;
 
 //Config seeds
