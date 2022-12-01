2f9eddc159774745948c558c21765107760d8d06
kelvynloreto <loretokelvyn@gmail.com>
2022-12-01 12:59:59 -0400

    edit input data

--- front/src/components/shipping/AddressClient.jsx
+++ front/src/components/shipping/AddressClient.jsx
@@ -1,54 +1,64 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useState } from 'react'
+// import dotenv from "dotenv";
 
-const AddressClient = () => {
+// dotenv.config();
+
+const AddressClient = ({setOriginUbigeo}) => {
 // peticiones get
   const [dataUbigeo, setDataUbigeo] = useState()
     const [countries, setCountries] = useState()
     const [regions, setRegion] = useState()
     const [provinces, setProvinces] = useState()
+    const [district, setDistrict] = useState()
 
 // selectors
     const [countrySelect, setCountrySelect] = useState()
     const [regionSelect, setRegionSelect] = useState()
     const [provinceSelect, seProvinceSelect] = useState()
+    const [districtSelect, setDistrictSelect] = useState()
 
 const getDataUbigeo = ()=>{
-    const url = "http://3.89.23.42:4000/api/v1/ubigeo/"
+    const url = "http://3.89.23.42:4001/api/v1/ubigeo/"
     axios.get(url)
     .then(res=>setDataUbigeo(res.data.ubigeo))
+    .catch(err=> console.log(err.message))
 
 }
 
 const getCountry = ()=>{
-    const url = "http://3.89.23.42:4000/api/v1/ubigeo/country"
+    const url =  
+    // `${process.env.REACT_APP_API_MOTHER}${process.env.REACT_APP_URL_UBIGEO_COUNTRIES}` ||
+     'http://3.89.23.42:4001/api/v1/ubigeo/country'
     axios.get(url)
     .then(res=>setCountries(res.data.ubigeo))
+    .catch(err=> console.log(err.message))
 
 }
-
 const getAllRegionsByCountry = ()=>{
-    const url = `http://3.89.23.42:4000/api/v1/ubigeo/region/${countrySelect}`
+    const url = `http://3.89.23.42:4001/api/v1/ubigeo/region/${countrySelect}`
     axios.get(url)
     .then(res=>setRegion(res.data.ubigeo))
+    .catch(err=> console.log(err.message))
 }
 const getAllProvinceSelectByRegion = ()=>{
-    const url = `http://3.89.23.42:4000/api/v1/ubigeo/province/${regionSelect}/`
+    const url = `http://3.89.23.42:4001/api/v1/ubigeo/province/${regionSelect}/`
     axios.get(url)
     .then(res=>setProvinces(res.data.ubigeo))
+    .catch(err=> console.log(err.message))
 }
-// if (dataUbigeo) {
-//   for (let i = 0; i < dataUbigeo.length; i++) {
-//    console.log(dataUbigeo[i].province    );
-    
-//   }
-// }
+const getAllDistrictsByregion= ()=>{
+    const url = `hhttp://3.89.23.42:4000/api/v1/ubigeo/district/${regionSelect}/`
+    axios.get(url)
+    .then(res=>setDistrict(res.data.ubigeo))
+    .catch(err=> console.log(err.message))
+}
 
 
 useEffect(() => {
     getCountry()
-    // getDataUbigeo()
+    getDataUbigeo()
 }, [])
 
 useEffect(() => {
@@ -57,6 +67,10 @@ useEffect(() => {
 
 useEffect(() => {
     getAllProvinceSelectByRegion()
+}, [regionSelect])
+
+useEffect(() => {
+  getAllDistrictsByregion()
 }, [regionSelect])
 
 
@@ -69,8 +83,9 @@ const handleRegion = () => {
 const handleProvince = () => {
     seProvinceSelect(event.target.value);
   };
-
-
+  const handleDistrict = ()=>{
+    setDistrictSelect(event.target.value)
+  }
 
   return (
     <div>
@@ -106,6 +121,17 @@ const handleProvince = () => {
            <option key={province.province
            } value={province.province}>
             {province.province}
+          </option>)}   
+        </select>
+        <select
+          id="district"
+          onChange={handleDistrict}
+        > 
+          <option hidden selected>Distrito</option>
+           {district?.map((district) =>           
+           <option key={district.id
+           } value={district.district}>
+            {district.district}
           </option>)}   
         </select>
 
