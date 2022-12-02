aa68d96d40758d165275ddd0c0d95e21b98b2a99
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-12-02 12:48:39 -0300

    Validacion search

--- front/src/components/routes/Search.jsx
+++ front/src/components/routes/Search.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { useState } from "react";
 
+
+
+const validacion = (input) =>{
+  let error = {}; 
+  if(!input || isNaN(input) !== true){
+      error.input = "Se requiere un numero de envío valido"
+  }
+  return error
+}
 
 const Search = () => {
+
+    const [input, setInput] = useState("")
+    const [error, setError]= useState({})
+
+    const handleInputChange = (e) =>{
+        e.preventDefault();
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setError()
+      setError(validacion(input))
+      const errorValidador = validacion(input)
+      if(Object.values(errorValidador).length !== 0){
+        alert("Se requiere un numero de envío valido")
+    }else{
+      const url = `url`;
+      axios
+        .get(url, input)
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) =>
+        console.log(err))
+        setInput('')
+    }
+ };
+
+
+    const onSubmit = (data) => {
+        console.log(data)
+};
+
+
     return(
         <div className='search'>
           <div className="search1">
@@ -13,8 +58,15 @@ const Search = () => {
               <option value= "sobi">Sobre internacional</option>
               <option value= "eco">E Commers</option>
             </select>
-          <input type="text" className="search__input br_button"  placeholder='Numero seguimiento'/>
-        <button className="search__button br_button">
+          <input  className="search__input br_button" 
+            value= {input}
+            type="text"
+            placeholder='Numero seguimiento'
+            onChange={(e)=>handleInputChange(e)}
+          />
+        <button className="search__button br_button"
+            type="submit"
+            onClick={(e)=>handleSubmit(e)}>
             <i className="fa-solid fa-magnifying-glass"></i>
         </button>
         </div>
