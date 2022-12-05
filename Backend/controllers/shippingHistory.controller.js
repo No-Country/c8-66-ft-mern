ef777f26c7009cda8c5a276d57a0660315e3789f
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-12-04 21:45:33 -0300

    feat:show shipping history

--- Backend/controllers/shippingHistory.controller.js
+++ Backend/controllers/shippingHistory.controller.js
@@ -8,9 +8,16 @@ const { catchAsync } = require('../utils/catchAsync.util');
 const { AppError } = require('../utils/appError.util');
 
 const showHistory = catchAsync(async (req,res,next) => {
-    const { shipping_id } = req.params.shipping_id;
+    const shipping_id  = req.params.shipping_id;
 
-    const shippingHistory = await ShippingStatusHistory.findAll();
+    console.log(shipping_id)
+
+    const shippingHistory = await ShippingStatusHistory.findAll(
+        {
+            where:{ shipping_id },
+            order:[["createdAt","DESC"]]
+        }
+    );
 
     if(shippingHistory){
         return res.status(200).json({
