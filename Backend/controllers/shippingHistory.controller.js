d6136f5efed81e0dafd89bc29f18ed858e6050d1
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-12-06 21:44:39 -0300

    fix:delete required

--- Backend/controllers/shippingHistory.controller.js
+++ Backend/controllers/shippingHistory.controller.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 // Models
 const { ShippingStatusHistory } = require('../database/shippingStatusHistory.model');
 const { Shipping } = require('../database/shipping.model');
